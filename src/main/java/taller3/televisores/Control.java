1ceddbc7f1ae689d5c6f65612663f59640d6d4f1
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-21 21:09:55 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,8 +1,45 @@
 package taller3.televisores;
 public class Control {
     TV tv;
-
-    public void enlazar(TV televisor){
-        this.tv = televisor;
+    
+    public TV getTv(){
+        return tv;
+        
     }
+    public void setTv (TV tv){
+        this.tv = tv;
+    }
+
+    public void enlazar(TV tele){
+        tv = tele;
+    }
+    public void turnOn(){
+        tv.estado = true;
+    }
+    public void turnOff(){
+        tv.estado = false;
+    }
+    public void canalUp() {
+        if (tv.estado && tv.getCanal() < 120) {
+            tv.canal += 1;
+        }
+    }
+    public void canalDown() {
+        if (tv.estado && 1 < tv.getCanal() && tv.getCanal() <= 120) {
+            tv.canal -= 1;
+        }
+    }
+
+    public void volumenUp(){
+        if( tv.getEstado() && tv.volumen < 7) {
+            tv.volumen += 1;
+        }
+    }
+    public void volumenDown(){
+        if (tv.getEstado() && tv.volumen > 0 && tv.volumen <= 7){
+            tv.volumen -=1;
+        }
+    }
+
+
 }
